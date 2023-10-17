4b809ce4d9d8c7583e5c0e5b53358f400dd1d177
aygame101 <aygame101@gmail.com>
2023-10-17 21:11:03 +0200

    Ajout et affichage OK, maintenant Arbo TO DO

---
+++ savequote.php
@@ -0,0 +1,10 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['quote'])) {
+    $quote = $_POST['quote'];
+    file_put_contents('temp.txt', $quote, FILE_APPEND | LOCK_EX);
+    echo 'Citation enregistrée avec succès.';
+} else {
+    header('HTTP/1.1 400 Bad Request');
+    echo 'Erreur lors de la requête.';
+}
+?>
--- script.js
+++ script.js
@@ -1,14 +1,62 @@
 document.addEventListener('DOMContentLoaded', function () {
     const line1Element = document.getElementById('line1');
     const line2Element = document.getElementById('line2');
+    const quoteForm = document.getElementById('quoteForm');
 
-    fetch('loadline.php')
-        .then(response => response.json())
-        .then(data => {
-            line1Element.textContent = data.line1;
-            line2Element.textContent = data.line2;
+    // Chargement initial d'une citation
+    loadRandomQuote();
+
+    // Écouter la soumission du formulaire
+    quoteForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const quote1Input = document.getElementById('quote1');
+        const quote2Input = document.getElementById('quote2');
+
+        const newQuote1 = quote1Input.value;
+        const newQuote2 = quote2Input.value;
+
+        // Enregistrer la nouvelle citation dans le fichier temp.txt
+        saveNewQuote(newQuote1, newQuote2);
+
+        // Réinitialiser les champs du formulaire
+        quote1Input.value = '';
+        quote2Input.value = '';
+
+        // Charger une nouvelle citation aléatoire
+        loadRandomQuote();
+    });
+
+    function loadRandomQuote() {
+        fetch('loadline.php')
+            .then(response => response.json())
+            .then(data => {
+                line1Element.textContent = data.line1;
+                line2Element.textContent = data.line2;
+            })
+            .catch(error => {
+                console.error('Erreur lors du chargement de la ligne :', error);
+            });
+    }
+
+    function saveNewQuote(quote1, quote2) {
+        const newQuote = `${quote1};${quote2};0\n`;
+        fetch('savequote.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: `quote=${encodeURIComponent(newQuote)}`,
         })
-        .catch(error => {
-            console.error('Erreur lors du chargement de la ligne :', error);
-        });
+            .then(response => {
+                if (response.ok) {
+                    console.log('Citation enregistrée avec succès.');
+                } else {
+                    console.error('Erreur lors de l\'enregistrement de la citation.');
+                }
+            })
+            .catch(error => {
+                console.error('Erreur lors de l\'enregistrement de la citation :', error);
+            });
+    }
 });
